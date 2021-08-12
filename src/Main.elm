a0ba84873b348c16f114e7b8cfaff60da51a30b7
zlodag <11688686+zlodag@users.noreply.github.com>
2021-08-12 22:43:22 +1200

    case list and detail views

--- src/Main.elm
+++ src/Main.elm
@@ -9,6 +9,7 @@ import Helper exposing (GraphqlRemoteData, toTime, viewData)
 import Html exposing (Html, a, b, dd, div, dl, dt, li, text, ul)
 import Html.Attributes exposing (href)
 import Predictions.Object.Case as Case
+import Predictions.Object.Diagnosis as Diagnosis
 import Predictions.Object.Group as Group
 import Predictions.Object.User as User
 import Predictions.Query as Query
@@ -50,6 +51,7 @@ type PossibleData
     | UserData (GraphqlRemoteData UserDetailResponse)
     | GroupList (GraphqlRemoteData GroupListResponse)
     | GroupData (GraphqlRemoteData GroupDetailResponse)
+    | CaseData (GraphqlRemoteData CaseDetailResponse)
     | NoData
 
 
@@ -148,49 +150,48 @@ displayMaybe displayFunction data =
 displayData : PossibleData -> Html msg
 displayData possibleData =
     case possibleData of
+        NoData ->
+            text "No data!"
+
         UserList graphqlRemoteData ->
-            viewData (displayNamedNodeList "/user/") graphqlRemoteData
+            viewData (displayNamedNodeList "/user") graphqlRemoteData
 
         UserData graphqlRemoteData ->
             viewData (displayMaybe displayUser) graphqlRemoteData
 
         GroupList graphqlRemoteData ->
-            viewData (displayNamedNodeList "/group/") graphqlRemoteData
+            viewData (displayNamedNodeList "/group") graphqlRemoteData
 
         GroupData graphqlRemoteData ->
             viewData (displayMaybe displayGroup) graphqlRemoteData
 
-        NoData ->
-            text "No data!"
+        CaseData graphqlRemoteData ->
+            viewData (displayMaybe displayCase) graphqlRemoteData
 
 
-displayNamedNodeListItem : String -> NamedNodeData -> Html msg
-displayNamedNodeListItem base_path node =
+displayNamedNodeLink : String -> NamedNodeData -> Html msg
+displayNamedNodeLink base_path node =
     case node.id of
         Id id ->
-            li [] [ a [ href <| base_path ++ id ] [ text node.name ] ]
+            a [ href <| base_path ++ "/" ++ id ] [ text node.name ]
 
 
 displayNamedNodeList : String -> List NamedNodeData -> Html msg
 displayNamedNodeList base_path nodes =
-    ul [] <| List.map (displayNamedNodeListItem base_path) nodes
+    ul [] <| List.map (li [] << List.singleton << displayNamedNodeLink base_path) nodes
 
 
 displayUser : UserDetailData -> Html msg
 displayUser user =
     dl []
         [ dt [] [ text "Name" ]
-        , dd []
-            [ case user.node.id of
-                Id id ->
-                    a [ href <| "/user/" ++ id ] [ text user.node.name ]
-            ]
+        , dd [] [ displayNamedNodeLink "/user" user.node ]
         , dt [] [ text "Created" ]
         , dd [] [ text <| toTime user.created ]
         , dt [] [ text <| "Groups (" ++ String.fromInt (List.length user.groups) ++ ")" ]
-        , dd [] [ displayNamedNodeList "/group/" user.groups ]
+        , dd [] [ displayNamedNodeList "/group" user.groups ]
         , dt [] [ text <| "Cases (" ++ String.fromInt (List.length user.cases) ++ ")" ]
-        , dd [] [ displayNamedNodeList "/case/" user.cases ]
+        , dd [] [ displayNamedNodeList "/case" user.cases ]
         ]
 
 
@@ -198,13 +199,32 @@ displayGroup : GroupDetailData -> Html msg
 displayGroup group =
     dl []
         [ dt [] [ text "Name" ]
-        , dd []
-            [ case group.node.id of
-                Id id ->
-                    a [ href <| "/group/" ++ id ] [ text group.node.name ]
-            ]
+        , dd [] [ displayNamedNodeLink "/group" group.node ]
         , dt [] [ text <| "Members (" ++ String.fromInt (List.length group.members) ++ ")" ]
-        , dd [] [ displayNamedNodeList "/user/" group.members ]
+        , dd [] [ displayNamedNodeList "/user" group.members ]
+        ]
+
+
+displayCase : CaseDetailData -> Html msg
+displayCase case_ =
+    dl []
+        [ dt [] [ text "Reference" ]
+        , dd [] [ displayNamedNodeLink "/case" case_.node ]
+        , dt [] [ text "Creator" ]
+        , dd [] [ displayNamedNodeLink "/user" case_.creator ]
+        , dt [] [ text <| "Diagnoses (" ++ String.fromInt (List.length case_.diagnoses) ++ ")" ]
+        , dd [] [ displayNamedNodeList "/diagnosis" case_.diagnoses ]
+        , dt [] [ text "Group" ]
+        , dd []
+            [ case case_.group of
+                Just group ->
+                    displayNamedNodeLink "/group" group
+
+                Nothing ->
+                    text "None"
+            ]
+        , dt [] [ text "Deadline" ]
+        , dd [] [ text <| toTime case_.deadline ]
         ]
 
 
@@ -218,6 +238,7 @@ type Route
     | User String
     | Groups
     | Group String
+    | Case String
 
 
 routeParser : Parser (Route -> a) a
@@ -227,6 +248,7 @@ routeParser =
         , map User <| s "user" </> string
         , map Groups <| s "groups"
         , map Group <| s "group" </> string
+        , map Case <| s "case" </> string
         ]
 
 
@@ -246,6 +268,9 @@ parseUrlAndRequest url =
 
                 Group string ->
                     Id string |> groupDetailQuery |> makeRequest (GroupData >> GotResponse)
+
+                Case string ->
+                    Id string |> caseDetailQuery |> makeRequest (CaseData >> GotResponse)
 
         Nothing ->
             Cmd.none
@@ -327,6 +352,33 @@ groupListQuery =
 
 type alias CaseLimitedData =
     NamedNodeData
+
+
+type alias CaseDetailData =
+    { node : NamedNodeData
+    , creator : NamedNodeData
+    , group : Maybe NamedNodeData
+    , deadline : Timestamp
+    , diagnoses : List NamedNodeData
+    }
+
+
+mapToCaseDetailData =
+    SelectionSet.map5 CaseDetailData
+        (SelectionSet.map2 NamedNodeData Case.id Case.reference)
+        (Case.creator <| SelectionSet.map2 NamedNodeData User.id User.name)
+        (Case.group <| SelectionSet.map2 NamedNodeData Group.id Group.name)
+        Case.deadline
+        (Case.diagnoses <| SelectionSet.map2 NamedNodeData Diagnosis.id Diagnosis.name)
+
+
+type alias CaseDetailResponse =
+    Maybe CaseDetailData
+
+
+caseDetailQuery : Id -> SelectionSet CaseDetailResponse RootQuery
+caseDetailQuery id =
+    Query.case_ { id = id } <| mapToCaseDetailData
 
 
 
